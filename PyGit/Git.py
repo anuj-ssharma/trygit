3f2fbe50398afd31155a6c09890cfbf8540e2ac4
Anuj Sharma <a.sharma@planittesting.com>
2017-11-09 10:56:31 +1300

    Feature-a changes

--- PyGit/Git.py
+++ PyGit/Git.py
@@ -2,3 +2,6 @@ class Git:
 
     def name(self):
         print("this is a new method created in the master branch")
+
+    def featurea(self):
+        print("This has been added as part of feature a changes")
